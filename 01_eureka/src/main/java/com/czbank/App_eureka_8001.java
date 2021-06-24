3a8d65b1254de2d5ed6d0a6567f34a0ae0abd33a
z464337866@163.com <z464337866@163.com>
2021-06-24 11:32:48 +0800

    add  01   config

---
+++ 01_eureka/src/main/java/com/czbank/App_eureka_8001.java
@@ -0,0 +1,12 @@
+package com.czbank;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App_eureka_8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(App_eureka_8001.class);
+    }
+}
